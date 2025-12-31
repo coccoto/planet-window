7b5c99f1975069bcf0b01729e445cec58f526209
coccoto <soprano.lune@gmail.com>
2025-12-31 18:29:50 +0900

    2025-12-31 18:29:50

--- client/src/entry-server.tsx
+++ client/src/entry-server.tsx
@@ -16,18 +16,13 @@ export default createHandler(() => (
                     <meta property='og:type' content='website'></meta>
                     <meta property='og:title' content='Planet Window<'></meta>
                     <meta property='og:description' content='Look out the window into space and explore the solar system in 3D. Rotate planets and discover the wonders of the cosmos.'></meta>
-                    <meta property='og:image' content='favicons/icon-512x512.png'></meta>
-                    {/* Twitter */}
-                    <meta name='twitter:card' content='summary'></meta>
-                    <meta name='twitter:creator' content='@coccoto'></meta>
+                    <meta property='og:image' content='favicons/favicon.png'></meta>
                     {/* Favicon */}
-                    <link rel='icon' type='image/png' href='favicons/icon-32x32.png'></link>
-                    <link rel='apple-touch-icon' href='favicons/icon-256x256.png'></link>
-
+                    <link rel='icon' type='image/svg+xml' href='favicons/favicon.svg'></link>
                     {/* Google Fonts */}
                     <link rel='preconnect' href='https://fonts.googleapis.com'></link>
-                    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous'></link>
-                    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;400;500&display=swap'></link>
+                    <link rel='preconnect' href='https://fonts.gstatic.com'></link>
+                    <link href='https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700&display=swap' rel='stylesheet'></link>
                 </head>
                 <body>
                     <div id="app">{children}</div>
