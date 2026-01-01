dce8d88bd637fe08eeb78e72aa57c8da3f4980df
coccoto <soprano.lune@gmail.com>
2026-01-02 04:47:19 +0900

    2026-01-02 04:47:19

--- server/core/logger.go
+++ server/core/logger.go
@@ -2,27 +2,18 @@ package core
 
 import (
 	"log/slog"
-	"sync"
 	"os"
-	"gopkg.in/natefinch/lumberjack.v2"
+	"sync"
 )
 
 var (
 	logger *slog.Logger
-	once sync.Once
+	once   sync.Once
 )
 
 func GetLogger() *slog.Logger {
-	once.Do(initLogger)
+	once.Do(func() {
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	})
 	return logger
-}
-
-func initLogger() {
-	logFile := &lumberjack.Logger{
-		Filename: os.Getenv("LOG_DIR") + "/app.log",
-		MaxSize: 1, // 最大サイズ (MB)
-		MaxBackups: 10, // 最大バックアップファイル数
-		Compress: true, // 圧縮
-	}
-	logger = slog.New(slog.NewJSONHandler(logFile, nil))
 }
